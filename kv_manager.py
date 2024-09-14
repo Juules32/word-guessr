b648f167bfbb413bfc6c781393da6fdad946dc37
Benjamin Jensen <Benjaminbyvej@gmail.com>
2024-09-14 17:16:25 +0200

    set up basic storage and retrieval methods

--- kv_manager.py
+++ kv_manager.py
@@ -1,3 +1,5 @@
+# kv_manager.py
+
 import os
 import redis
 from dotenv import load_dotenv
@@ -10,13 +12,44 @@ class KV:
         self.url = os.getenv("KV_URL", "redis://localhost:6379")
         if url:
             self.url = url
+        self.max_guesses = 6
+        self.rc = redis.from_url(url=self.url) # redis client
+
+    def set_puzzle(self, date: str, puzzle_data: dict) -> bool:
+        return self.rc.hset("puzzle", key=date, value=str(puzzle_data))
+
+    def get_puzzle(self, date: str) -> dict:
+        result = self.rc.hget(name="puzzle", key=date)
+        if not result:
+            raise Exception(f"Puzzle: {date} not found!")
+        return eval(result)
+
+    def get_puzzles(self) -> dict:
+        result = self.rc.hgetall("puzzle")
+        return {k.decode(): eval(v) for k, v in result.items()}
+
+    def set_user_progress(self, uid: str, date: str, progress: dict) -> bool:
+        return self.rc.hset(f"user:{uid}:puzzle", key=date, value=str(progress))
+
+    def get_user_progress(self, uid: str, date: str) -> dict:
+        result = self.rc.hget(f"user:{uid}:puzzle", key=date)
+        return eval(result) if result else {"guesses": 0, "completed": False, "won": False}
+    
+    def get_user_puzzles(self, uid: str) -> dict:
+        result = self.rc.hgetall(f"user:{uid}:puzzle")
+        return {k.decode(): eval(v) for k, v in result.items()}
+    
+    def guess(self, date: str, uid: str, guess: str) -> None:
+        # Get existing data
+        progress = self.get_user_progress(uid, date)
+        puzzle = self.get_puzzle(date)
+        solution = puzzle.get("solution")
         
-        self.redis_client = redis.from_url(url=self.url)
+        # Determine new user data
+        guesses = progress["guesses"] + 1
+        completed = True if guess == solution or guesses >= self.max_guesses else False
+        won = True if guess == solution else False
+        progress = {"guesses": guesses, "completed": completed, "won": won}
 
-    def set(self, key, value) -> bool:
-        result = self.redis_client.set(key, value)
-        return result
-
-    def get(self, key) -> bool:
-        result = self.redis_client.get(key)
-        return result.decode()
+        # Set new user data
+        return self.set_user_progress(uid, date, progress)
--- main.py
+++ main.py
@@ -1,5 +1,32 @@
-from vercel_kv import KV
+# main.py
+
+from kv_manager import KV
 
 kv = KV()
-print(kv.set("test", "test local redis"))
-print(kv.get("test"))
+
+# Example usage
+date = "2024-09-15"
+date2 = "2024-09-17"
+date3 = "2024-09-18"
+puzzle_data = {
+    "date": "...",
+    "length of word": 6,
+    "word type": "noun",
+    "synonym(s)": "synonym",
+    "definition": "definition",
+    "pronunciation": "file_path",
+    "solution": "solution_word"
+}
+
+# Set and get puzzle
+kv.set_puzzle(date, puzzle_data)
+kv.set_puzzle(date2, puzzle_data)
+kv.set_puzzle(date3, puzzle_data)
+
+uid = "user_1"
+uid2 = "user_2"
+
+kv.guess(date2, uid, "bad_word")
+print(kv.get_user_progress(uid, date2))
+print(kv.get_user_puzzles(uid))
+print(kv.get_puzzles()["2024-09-18"])

e00d1b2c04e717eeb1c7ed3ac12c7abeab019d26
Benjamin Jensen <Benjaminbyvej@gmail.com>
2024-09-14 19:43:26 +0200

    pydantic

--- kv_manager.py
+++ kv_manager.py
@@ -2,54 +2,89 @@
 
 import os
 import redis
-from dotenv import load_dotenv
+from pydantic import BaseModel
 
-# Custom path can be given to experiment locally with different env vars.
-load_dotenv()
+# Constants
+MAX_GUESSES = 6
+
+class Puzzle(BaseModel):
+    date: str
+    word_length: int
+    word_type: str
+    synonym: str
+    definition: str
+    pronunciation: str # Should be file
+    solution: str
+
+class UserProgress(BaseModel):
+    guesses: int = 0
+    completed: bool = False
+    won: bool = False
 
 class KV:
     def __init__(self, url: str = None):
         self.url = os.getenv("KV_URL", "redis://localhost:6379")
         if url:
             self.url = url
-        self.max_guesses = 6
         self.rc = redis.from_url(url=self.url) # redis client
 
-    def set_puzzle(self, date: str, puzzle_data: dict) -> bool:
-        return self.rc.hset("puzzle", key=date, value=str(puzzle_data))
+    def set_puzzle(self, date: str, puzzle: Puzzle) -> bool:
+        return self.rc.hset("puzzle", key=date, value=puzzle.model_dump_json())
 
-    def get_puzzle(self, date: str) -> dict:
+    def get_puzzle(self, date: str) -> Puzzle:
         result = self.rc.hget(name="puzzle", key=date)
         if not result:
             raise Exception(f"Puzzle: {date} not found!")
-        return eval(result)
+        return Puzzle.model_validate_json(result)
 
-    def get_puzzles(self) -> dict:
+    def set_puzzles(self, puzzles: dict[str, Puzzle]) -> bool:
+        for date, puzzle in puzzles.items():
+            self.set_puzzle(date=date, puzzle=puzzle)
+
+    def get_puzzles(self) -> dict[str, Puzzle]:
         result = self.rc.hgetall("puzzle")
-        return {k.decode(): eval(v) for k, v in result.items()}
+        return {k.decode(): Puzzle.model_validate_json(v) for k, v in result.items()}
 
-    def set_user_progress(self, uid: str, date: str, progress: dict) -> bool:
-        return self.rc.hset(f"user:{uid}:puzzle", key=date, value=str(progress))
+    def set_user_progress(self, uid: str, date: str, progress: UserProgress) -> bool:
+        return self.rc.hset(f"user:{uid}:puzzle", key=date, value=progress.model_dump_json())
 
-    def get_user_progress(self, uid: str, date: str) -> dict:
-        result = self.rc.hget(f"user:{uid}:puzzle", key=date)
-        return eval(result) if result else {"guesses": 0, "completed": False, "won": False}
+    def get_user_progress(self, uid: str, date: str) -> UserProgress:
+        result: UserProgress = self.rc.hget(f"user:{uid}:puzzle", key=date)
+        return UserProgress.model_validate_json(result) if result else UserProgress()
     
-    def get_user_puzzles(self, uid: str) -> dict:
+    def get_user_puzzles(self, uid: str) -> dict[str, UserProgress]:
         result = self.rc.hgetall(f"user:{uid}:puzzle")
-        return {k.decode(): eval(v) for k, v in result.items()}
+        return {k.decode(): UserProgress.model_validate_json(v) for k, v in result.items()}
     
-    def guess(self, date: str, uid: str, guess: str) -> None:
+    def guess(self, date: str, uid: str, guess: str) -> bool:
         # Get existing data
         progress = self.get_user_progress(uid, date)
         puzzle = self.get_puzzle(date)
-        solution = puzzle.get("solution")
+        solution = puzzle.solution
         
         # Determine new user data
-        guesses = progress["guesses"] + 1
-        completed = True if guess == solution or guesses >= self.max_guesses else False
-        won = True if guess == solution else False
-        progress = {"guesses": guesses, "completed": completed, "won": won}
+        guesses = progress.guesses + 1
+        completed = guess == solution or guesses >= MAX_GUESSES
+        won = guess == solution
+        progress = UserProgress(guesses=guesses, completed=completed, won=won)
 
         # Set new user data
         return self.set_user_progress(uid, date, progress)
+
+    def get_user_stats(self, uid: str) -> dict:
+        puzzle_data = self.get_user_puzzles(uid)
+
+        stats = {str(i): 0 for i in range(1, MAX_GUESSES + 1)}
+        stats["More"] = 0
+
+        for puzzle in puzzle_data.values():
+            if not puzzle.completed:
+                continue
+            
+            guesses = puzzle.guesses
+            if guesses <= MAX_GUESSES:
+                stats[str(guesses)] += 1
+            else:
+                stats["More"] += 1
+        
+        return stats
--- main.py
+++ main.py
@@ -1,32 +1,68 @@
 # main.py
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+from kv_manager import KV, Puzzle
 
-from kv_manager import KV
+# Custom path can be given to experiment locally with different env vars.
+load_dotenv()
 
 kv = KV()
 
+def get_date_str(delta: int = 0) -> str:
+    today = datetime.today()
+    date = today + timedelta(days=delta)
+    return date.strftime('%Y-%m-%d')
+
 # Example usage
-date = "2024-09-15"
-date2 = "2024-09-17"
-date3 = "2024-09-18"
-puzzle_data = {
-    "date": "...",
-    "length of word": 6,
-    "word type": "noun",
-    "synonym(s)": "synonym",
-    "definition": "definition",
-    "pronunciation": "file_path",
-    "solution": "solution_word"
+date1 = get_date_str()
+date2 = get_date_str(1)
+date3 = get_date_str(2)
+
+puzzle1 = Puzzle(
+    date=date1, 
+    word_length=4,
+    word_type="verb",
+    synonym="tread",
+    definition="to move along on foot : advance by steps",
+    pronunciation="file_path",
+    solution="walk"
+)
+
+puzzle2 = Puzzle(
+    date=date2, 
+    word_length=4,
+    word_type="adjective",
+    synonym="tranquil",
+    definition="a period or condition of freedom from storms, high winds, or rough activity of water",
+    pronunciation="file_path",
+    solution="calm"
+)
+
+puzzle3 = Puzzle(
+    date=date3, 
+    word_length=4,
+    word_type="noun",
+    synonym="tiny",
+    definition="an insignificant or tiny person",
+    pronunciation="file_path",
+    solution="peanut"
+)
+
+puzzles = {
+    date1: puzzle1,
+    date2: puzzle2,
+    date3: puzzle3
 }
 
 # Set and get puzzle
-kv.set_puzzle(date, puzzle_data)
-kv.set_puzzle(date2, puzzle_data)
-kv.set_puzzle(date3, puzzle_data)
+kv.set_puzzles(puzzles=puzzles)
 
 uid = "user_1"
 uid2 = "user_2"
 
-kv.guess(date2, uid, "bad_word")
-print(kv.get_user_progress(uid, date2))
+kv.guess(date2, uid, "calm")
+
+print(kv.get_user_progress(uid, date1))
 print(kv.get_user_puzzles(uid))
-print(kv.get_puzzles()["2024-09-18"])
+print(kv.get_puzzle(date3))
+print(kv.get_user_stats(uid))

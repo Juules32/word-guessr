f5346aef168b845bf8bb35d6e472c5558e88bebf
Benjamin Jensen <Benjaminbyvej@gmail.com>
2024-09-17 19:36:32 +0200

    added some more template puzzles

--- example_data.py
+++ example_data.py
@@ -7,9 +7,11 @@ from util.date import get_date_str
 date1 = get_date_str()
 date2 = get_date_str(1)
 date3 = get_date_str(2)
+date4 = get_date_str(3)
+date5 = get_date_str(4)
 
 puzzle1 = Puzzle(
-    date=date1, 
+    date=date1,
     word_length=4,
     word_type="verb",
     synonym="tread",
@@ -19,7 +21,7 @@ puzzle1 = Puzzle(
 )
 
 puzzle2 = Puzzle(
-    date=date2, 
+    date=date2,
     word_length=4,
     word_type="adjective",
     synonym="tranquil",
@@ -29,7 +31,7 @@ puzzle2 = Puzzle(
 )
 
 puzzle3 = Puzzle(
-    date=date3, 
+    date=date3,
     word_length=4,
     word_type="noun",
     synonym="tiny",
@@ -38,8 +40,30 @@ puzzle3 = Puzzle(
     solution="peanut"
 )
 
+puzzle4 = Puzzle(
+    date=date4,
+    word_length=9,
+    word_type="adjective",
+    synonym="debatable",
+    definition="subject to two or more interpretations and usually used to mislead or confuse",
+    pronunciation="file_path",
+    solution="equivocal"
+)
+
+puzzle5 = Puzzle(
+    date=date5,
+    word_length=10,
+    word_type="noun",
+    synonym="dishonesty",
+    definition="lack of honesty or integrity : disposition to defraud or deceive",
+    pronunciation="file_path",
+    solution="dishonesty"
+)
+
 puzzles = {
     date1: puzzle1,
     date2: puzzle2,
-    date3: puzzle3
+    date3: puzzle3,
+    date4: puzzle4,
+    date5: puzzle5
 }
